2d039b6672cd7122b2a25f57fc3e6268bfeb9841
Israel Arvizu <israelarvizuofficial@gmail.com>
2022-06-28 14:20:49 -0700

    dashboard

--- react-app/src/components/Dashboard.js
+++ react-app/src/components/Dashboard.js
@@ -1,14 +1,46 @@
 import {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import { loadHomeNews } from '../store/news';
+
 
 function Dashboard() {
+    const dispatch = useDispatch()
+    const newsArticles = useSelector(state => state.newsReducer.news);
+    useEffect(() => {
+        dispatch(loadHomeNews())
+    }, [dispatch])
+
+    if(newsArticles === undefined) return <h2>Loading</h2>
+    // console.log(newsArticles[0].thumbnail.resolutions[2].url);
     return(
         <>
             <div>
                 LeftSection
-                <div>Portfolion Graph</div>
-                <div>Buying Power</div>
-                <div>Trending Lists</div>
-                <div>News</div>
+                <h2>Portfolion Graph</h2>
+                <h2>Buying Power</h2>
+                <h2>Trending Lists</h2>
+                    <div>
+
+                    </div>
+                <h2>News</h2>
+                {newsArticles.map((article) => {
+                    if(article.thumbnail !== undefined)
+                        return (
+                            <div>
+                                <hr></hr>
+                                <a href={article.link}>
+                                    <img src={article.thumbnail.resolutions[1].url} alt='thumbnail' />
+                                    <a>{article.publisher}</a>
+                                    <h3>{article.title}</h3>
+                                </a>
+                                <div>
+                                    <p>{article.relatedTickers[0]}</p>
+                                    <p>{article.relatedTickers[1]}</p>
+                                </div>
+                            </div>
+                        )
+                    return null
+                })}
             </div>
             <div>
                 WatchList
