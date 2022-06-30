5fd8cd0f6d4e23d5234f611df84a3626bf21bdb7
Brian <brianshkim25@gmail.com>
2022-06-30 13:03:19 -0700

    typo

--- react-app/src/store/list.js
+++ react-app/src/store/list.js
@@ -38,7 +38,7 @@ export const addstock = (stock) => async(dispatch)=>{
     {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(list)
+        body: JSON.stringify(stock)
     })
     const data = await response.json()
     dispatch(addstocktolist(data));
