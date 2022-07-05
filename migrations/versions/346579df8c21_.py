d237c8c049ef18d745696b3792bb09e373cc7e12
Israel Arvizu <israelarvizuofficial@gmail.com>
2022-07-04 18:42:59 -0700

    heroku db update

--- migrations/versions/346579df8c21_.py
+++ migrations/versions/346579df8c21_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: abc5b801d154
+Revision ID: 346579df8c21
 Revises: 
-Create Date: 2022-07-04 13:11:43.331963
+Create Date: 2022-07-04 18:42:19.409105
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'abc5b801d154'
+revision = '346579df8c21'
 down_revision = None
 branch_labels = None
 depends_on = None
