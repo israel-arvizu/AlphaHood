9deb963ee0dbdfcaae9089f5a56ed8b44b6f475a
Israel Arvizu <israelarvizuofficial@gmail.com>
2022-09-27 00:59:15 -0700

    signup css layout redone

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -17,42 +17,36 @@ const SignUpForm = () => {
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
-
-  useEffect(()=>{
-      let newErrors = []
-      if (username === "") {
-        newErrors.push('Name cant be blank!')
-      }
-      if (username.length < 3){
-        newErrors.push("username must be at least 3 characters")
-      }
-
-      if (!email.includes("@")){
-        newErrors.push('Enter a valid email')
-      }
-
-      if(email === "") {
-        newErrors.push('Email is a mandatory Field')
-      }
-
-      if (password.length < 5){
-        newErrors.push("password must be greater than 5 characters")
-      }
-      if (password !== repeatPassword) {
-        newErrors.push("Password should match")
-      }
-
-      if (!birthday){
-        newErrors.push("Enter Birthday")
-      }
-
-      setErrors(newErrors)
-
-
-  }, [username,email, password, repeatPassword] )
-
   const onSignUp = async (e) => {
     e.preventDefault();
+    let newErrors = []
+    if (username === "") {
+      newErrors.push('Name cant be blank!')
+    }
+    if (username.length < 3){
+      newErrors.push("Username must be at least 3 characters")
+    }
+
+    if (!email.includes("@")){
+      newErrors.push('Enter a valid email')
+    }
+
+    if(email === "") {
+      newErrors.push('Email is a mandatory Field')
+    }
+
+    if (password.length < 5){
+      newErrors.push("Password must be greater than 5 characters")
+    }
+    if (password !== repeatPassword) {
+      newErrors.push("Password should match")
+    }
+
+    if (!birthday){
+      newErrors.push("Enter Birthday")
+    }
+
+    setErrors(newErrors)
     if (password === repeatPassword) {
       const data = await dispatch(signUp(username, email, birthday, password));
       if (data) {
@@ -82,80 +76,76 @@ const SignUpForm = () => {
   }
 
   if (user) {
-    history.push('/');
+    history.push('/dashboard');
   }
   return (
-    <div className = 'signup-page'>
-      <div className = 'background-color'/>
-        <h1 className= 'signup-header'>
-          Create your Login
-        </h1>
-        <p className= 'signup-desc'>
-      We'll need your name, email address, and a unique password. You'll use this login to access Robinhood next time.
-      </p>
-        <div className = 'signup-img'>
-            <img src="https://cdn.robinhood.com/app_assets/odyssey/rockets.png" id="signuprocket" alt="rockets"/>
+    <div className='signup-page-main-outline'>
+      <div className='right-section-signup'>
+        <div className='signup-header-container'>
+          <h1 className='signup-header'>
+            Create your Login
+          </h1>
+          <p>
+            To access AlphaHood and all its features you'll need to make an account.
+          </p>
+          <p>You'll use this login to access AlphaHood next time.</p>
+          <p id='sign-up-warning-msg'>Please note this is a not a real trading website!</p>
         </div>
-    <form className = 'signup-form'
-    onSubmit={onSignUp}>
-      <div className = 'signup-instructions'>
-        Enter your first and last name as they appear on your government ID.
+        <img src="https://cdn.robinhood.com/app_assets/odyssey/rockets.png" id="signup-rocket" alt="rockets"/>
       </div>
-      <div className = 'inputs'>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+      <div className='left-section-signup'>
+        <form className='signup-form' onSubmit={onSignUp}>
+          <p className='signup-instructions'>
+            Welcome! Please fill out your information below,
+          </p>
+          <div className='inputs-container'>
+            <div className='errors-content-container'>
+              {errors.map((error, ind) => (
+                <p key={ind}>{error}</p>
+              ))}
+            </div>
+              <input
+                type='text'
+                className='username'
+                onChange={updateUsername}
+                value={username}
+                placeholder="User Name"
+              ></input>
+              <input
+                type='text'
+                className='email'
+                onChange={updateEmail}
+                value={email}
+                placeholder="Email"
+              ></input>
+              <div>
+                <label className='birthday-label'>Birthday</label>
+              </div>
+              <input
+              type='date'
+              className='birthday'
+              onChange={updateBirthday}
+              value={birthday}
+              ></input>
+              <input
+                type='password'
+                className='password'
+                onChange={updatePassword}
+                value={password}
+                placeholder="Password"
+              ></input>
+              <input
+                type='password'
+                className='repeat_password'
+                onChange={updateRepeatPassword}
+                value={repeatPassword}
+                required={true}
+                placeholder="Repeat Password"
+              ></input>
+            <button className='signupsubmit-button' type='submit' disabled={!!errors.length}>Sign Up</button>
+          </div>
+        </form>
       </div>
-      <div>
-        <input
-          type='text'
-          className='username'
-          onChange={updateUsername}
-          value={username}
-          placeholder="User Name"
-        ></input>
-      </div>
-      <div>
-        <input
-          type='text'
-          className='email'
-          onChange={updateEmail}
-          value={email}
-          placeholder="Email"
-        ></input>
-      </div>
-      <div>
-        <label className='birthday-label'>Birthday</label>
-        <input
-        type='date'
-        className='birthday'
-        onChange={updateBirthday}
-        value={birthday}
-        ></input>
-      </div>
-<div>
-        <input
-          type='password'
-          className='password'
-          onChange={updatePassword}
-          value={password}
-          placeholder="Password"
-        ></input>
-      </div>
-      <div>
-        <input
-          type='password'
-          className='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-          placeholder="Repeat Password"
-        ></input>
-      </div>
-        <button className= 'signupsubmit-button' type='submit' disabled={!!errors.length}>Sign Up</button>
-      </div>
-    </form>
     </div>
   );
 };
